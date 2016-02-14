5ac72b60d7b42898412e614c05f580f351310b35
Gerik Peterson <GGerikP1@Gmail.com>
2016-02-14 19:08:56 +0000

    Refactored the landing / index page and added a environmental config system.

--- gerikpeterson/src/main/node/routes/index.js
+++ gerikpeterson/src/main/node/routes/index.js
@@ -4,7 +4,7 @@ var config = require('config');
 var configs = config.get('app');
 
 /* GET home page. */
-router.get('/:id', function(req, res, next) {
+router.get('/(:id)?', function(req, res, next) {
 	var id = req.params.id;
 	if (id === "controller") {
 		res.render(
